eefae83fab414a505a6dbaee5cf7e3b7eb8c9686
SmartMonkey <sk3250@mail.ru>
2020-05-31 23:13:55 +0500

    remade ws server

--- server/server_prod.js
+++ server/server_prod.js
@@ -1,13 +1,15 @@
 // const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const https = require('http');
+// const https = require('https');
+const http = require('http');
+const basicAuth = require('express-basic-auth');
 const server = require('./ws');
 
 
 const DIST_DIR = path.join(__dirname, '../dist');
-const externalPort = process.env.PORT || 80;
-const internalPort = 8082;
+const externalPort = process.env.PORT || 8080;
+const internalPort = 8083;
 // const httpsOptions = {
 //   cert: fs.readFileSync(path.join(__dirname, '/certs/localhost.crt')),
 //   key: fs.readFileSync(path.join(__dirname, '/certs/device.key')),
@@ -16,16 +18,22 @@ const internalPort = 8082;
 const app = express();
 app.disable('x-powered-by');
 
+const auth = basicAuth({
+  users: { admin: 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp',
+});
+
 
 app.use((req, res, next) => {
   console.log(req.url);
   next();
 });
 
-app.use(express.static(DIST_DIR));
+app.use(auth, express.static(DIST_DIR));
 
 
-const httpServer = https.createServer(app);
+const httpServer = http.createServer(app);
 
 httpServer.listen(externalPort, () => {
   console.log(`Server listening on port ${externalPort}!`);
--- server/ws.js
+++ server/ws.js
@@ -27,79 +27,73 @@ module.exports = (internalPort, httpServer) => {
     console.log('no saved state');
   }
 
-  // httpServer.listen(externalPort, () => {
-  //   console.log(`Server listening on port ${externalPort}!`);
-  // });
+  const clients = [];
+  const controllers = [];
 
   const extWss = new WebSocket.Server({ server: httpServer });
-  const innerWss = new WebSocket.Server({ port: internalPort });
+  // const innerWss = new WebSocket.Server({ port: internalPort });
 
-  const sendMessage = (wsServer, data) => {
-    wsServer.clients.forEach((client) => {
+  const sendMessage = (connectionsList, data) => {
+    connectionsList.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
   };
 
-  extWss.on('connection', (ws) => {
+  extWss.on('connection', (ws, req) => {
     const clientId = _.uniqueId();
     ws.id = clientId;
-    extWss.clients.forEach((client) => {
-      if (client.id !== clientId) {
-        client.terminate();
-      }
-    });
-    ws.send(JSON.stringify({ type: 'serverState', data: state }));
-
-    ws.on('message', (msg) => {
-      const data = JSON.parse(msg);
-      state.required = data;
-
-      fs.writeFile(SAVED_STATE_DIR, msg, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('The file has been saved!');
-        }
-      });
-
-      console.log(`ext message\n ${JSON.stringify(state)}`);
-      sendMessage(innerWss, msg);
-    });
-
-    ws.on('close', () => {
-      console.log('client disconnected');
-    });
-
-    ws.on('error', (err) => {
-      console.log(err.message);
-    });
-  });
-
-
-  innerWss.on('connection', (ws, req) => {
-    const controllerId = _.uniqueId();
     ws.isAlive = true;
-    ws.id = controllerId;
-    if (state.controllerConnection === 'online' && req.headers.origin === 'Espruino') {
-      innerWss.clients.forEach((client) => {
-        if (client.id !== controllerId) {
-          client.terminate();
+    if (req.headers.origin === 'Espruino') {
+      if (state.controllerConnection === 'online') {
+        controllers.forEach((c) => {
+          if (c.id !== clientId) {
+            c.terminate();
+          }
+        });
+        console.log('old controller connection was terminated');
+      }
+      ws.type = 'controller';
+      console.log('controller connected');
+      state.controllerConnection = 'online';
+      ws.send(JSON.stringify(state.required));
+      sendMessage(clients, JSON.stringify({ type: 'connectionState', data: state.controllerConnection }));
+      controllers.push(ws);
+    } else {
+      clients.forEach((c) => {
+        if (c.id !== clientId) {
+          c.terminate();
         }
       });
-      console.log('old controller connection was terminated');
+      ws.type = 'client';
+      ws.send(JSON.stringify({ type: 'serverState', data: state }));
+      clients.push(ws);
     }
-    console.log('controller connected');
-    state.controllerConnection = 'online';
-    ws.send(JSON.stringify(state.required));
-    sendMessage(extWss, JSON.stringify({ type: 'connectionState', data: state.controllerConnection }));
 
     ws.on('message', (msg) => {
-      const data = JSON.parse(msg);
-      state.current = data;
-      console.log(`inner message\n ${JSON.stringify(state)}`);
-      sendMessage(extWss, JSON.stringify({ type: 'currentTemp', data }));
+      if (ws.type === 'client') {
+        const data = JSON.parse(msg);
+        state.required = data;
+        fs.writeFile(SAVED_STATE_DIR, msg, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('The file has been saved!');
+          }
+        });
+        console.log(`ext message\n ${JSON.stringify(state)}`);
+        sendMessage(controllers, msg);
+
+        return;
+      } if (ws.type === 'controller') {
+        const data = JSON.parse(msg);
+        state.current = data;
+        console.log(`inner message\n ${JSON.stringify(state)}`);
+        sendMessage(clients, JSON.stringify({ type: 'currentTemp', data }));
+        return;
+      }
+      console.log(`unknown ws connection type ${ws.type}`);
     });
 
     ws.on('pong', () => {
@@ -107,7 +101,7 @@ module.exports = (internalPort, httpServer) => {
     });
 
     const interval = setInterval(() => {
-      innerWss.clients.forEach((client) => {
+      controllers.forEach((client) => {
         if (!client.isAlive) {
           console.log('controller not Alive');
           client.terminate();
@@ -119,13 +113,18 @@ module.exports = (internalPort, httpServer) => {
     }, 10000);
 
     ws.on('close', () => {
-      clearInterval(interval);
-      if (innerWss.clients.size === 0) {
-        state.controllerConnection = 'offline';
-        state.current.temp = '';
-        state.current.on = false;
-        console.log('controller disconnected');
-        sendMessage(extWss, JSON.stringify({ type: 'serverState', data: state }));
+      if (ws.type === 'client') {
+        console.log('client disconnected');
+        return;
+      } if (ws.type === 'controller') {
+        clearInterval(interval);
+        if (controllers.length === 0) {
+          state.controllerConnection = 'offline';
+          state.current.temp = '';
+          state.current.on = false;
+          console.log('controller disconnected');
+          sendMessage(clients, JSON.stringify({ type: 'serverState', data: state }));
+        }
       }
     });
 
@@ -134,3 +133,62 @@ module.exports = (internalPort, httpServer) => {
     });
   });
 };
+
+
+//   innerWss.on('connection', (ws, req) => {
+//     const controllerId = _.uniqueId();
+//     ws.isAlive = true;
+//     ws.id = controllerId;
+//     if (state.controllerConnection === 'online' && req.headers.origin === 'Espruino') {
+//       innerWss.clients.forEach((client) => {
+//         if (client.id !== controllerId) {
+//           client.terminate();
+//         }
+//       });
+//       console.log('old controller connection was terminated');
+//     }
+//     console.log('controller connected');
+//     state.controllerConnection = 'online';
+//     ws.send(JSON.stringify(state.required));
+//     sendMessage(extWss,
+//  JSON.stringify({ type: 'connectionState', data: state.controllerConnection }));
+
+//     ws.on('message', (msg) => {
+//       const data = JSON.parse(msg);
+//       state.current = data;
+//       console.log(`inner message\n ${JSON.stringify(state)}`);
+//       sendMessage(extWss, JSON.stringify({ type: 'currentTemp', data }));
+//     });
+
+//     ws.on('pong', () => {
+//       ws.isAlive = true;
+//     });
+
+//     const interval = setInterval(() => {
+//       innerWss.clients.forEach((client) => {
+//         if (!client.isAlive) {
+//           console.log('controller not Alive');
+//           client.terminate();
+//         } else {
+//           client.isAlive = false;
+//           client.ping(() => { });
+//         }
+//       });
+//     }, 10000);
+
+//     ws.on('close', () => {
+//       clearInterval(interval);
+//       if (innerWss.clients.size === 0) {
+//         state.controllerConnection = 'offline';
+//         state.current.temp = '';
+//         state.current.on = false;
+//         console.log('controller disconnected');
+//         sendMessage(extWss, JSON.stringify({ type: 'serverState', data: state }));
+//       }
+//     });
+
+//     ws.on('error', (err) => {
+//       console.log(err.message);
+//     });
+//   });
+// };
